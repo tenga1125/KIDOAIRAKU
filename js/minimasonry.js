458619d5bb106e41f1002d2f8e0a653a80578a35
tenga1125 <yamazakitenga@gmail.com>
2020-11-10 10:33:51 +0900

    3nd commit

--- js/minimasonry.js
+++ js/minimasonry.js
@@ -1,16 +1,20 @@
-$(function () {
+// $(function () {
   // var exampleDefault = new MiniMasonry({
   //   container: '.masonry',
   //   baseWidth: 260,
   //   gutter: 10,
   // });
 
-  let magicGrid = new MagicGrid({
-    container: '.container',
-    animate: true,
-    gutter: 10,
-    static: true,
-    useMin: true
+  $('.container').imagesLoaded( function() {
+    // 画像を読み込んだ後に実行したい処理
+    let magicGrid = new MagicGrid({
+      container: '.container',
+      animate: true,
+      gutter: 10,
+      static: true,
+      useMin: true
+    });
+    magicGrid.listen();
   });
-  magicGrid.listen();
-});
+
+// });
